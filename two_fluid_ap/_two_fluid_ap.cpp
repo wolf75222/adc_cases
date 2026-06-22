22565229efd18f8d0a42ccad68d0fdf88c74153f
Romain Despoullains <46008114+wolf75222@users.noreply.github.com>
2026-06-22 16:27:07 +0200

    ADC-392 Repoint two_fluid_ap includes for the include/adc reorg
    Companion to adc_cpp ADC-392/ADC-394.

--- two_fluid_ap/_two_fluid_ap.cpp
+++ two_fluid_ap/_two_fluid_ap.cpp
@@ -14,8 +14,8 @@
 
 #include "case_export.h"  // ADC_CASE_EXPORT : dllexport portable des entry points (common/, via -I de native.py)
 
-#include <adc/numerics/elliptic/geometric_mg.hpp>
-#include <adc/mesh/for_each.hpp>  // device_fence
+#include <adc/numerics/elliptic/mg/geometric_mg.hpp>
+#include <adc/mesh/execution/for_each.hpp>  // device_fence
 
 #include <cmath>
 #include <cstddef>
